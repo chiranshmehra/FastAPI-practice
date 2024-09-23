a7eaa999125a2eba11f9dd7fd42260911c255cc2
chiranshmehra <chiranshmehra1@gmail.com>
2024-09-23 18:50:11 +0530

    added db

--- main.py
+++ main.py
@@ -1,16 +1,50 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import Optional
+import uvicorn
+
+
 
 app = FastAPI()
 
 
-@app.get('/')
-def index():
-    return {'data': {
-        'name': 'chriansh'
-    }}
+@app.get('/blog')
+def index(limit = 10, published: bool = True):
+    if published:
+        return {'data': f'{limit} published blogs from the db'}
+    else:
+        return {'data': f'{limit} blogs form the db'}
 
-@app.get('/about')
-def about():
+@app.get('/blog/unpublished')
+def unpublished():
+    return {'data': 'unpublished'}
+ 
+
+
+@app.get('/blog/{id}')
+def showBlog(id: int):
     return {
-        'data':'about page'
+        'data': id
     }
+
+
+@app.get('/blog/{id}/comments')
+def comments(id):
+
+    return {'data':{'1','2'}}
+
+
+class Blog(BaseModel):
+    title: str
+    body: str
+    publishedAt: Optional[bool]
+
+
+
+@app.post('/blog')
+def createBlog(blog: Blog): 
+    return {'data': f"Blog is created with title as {blog.title}"}
+
+
+
+    
